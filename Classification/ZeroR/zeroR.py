b7d9cec6aae04e109091ecbcca38259b565d5b57
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2016-02-19 00:09:03 -0500

    Fix small accuracy bug

--- Classification/ZeroR/zeroR.py
+++ Classification/ZeroR/zeroR.py
@@ -13,13 +13,13 @@ from StringIO import StringIO
 # with open(file_dir, 'r') as data: raw_data = data.read()
 # data = np.genfromtxt(StringIO(raw_data), delimiter=',', dtype="|S10")
 
+# Kidney Disease Sample Dataset
 file_dir = '../../datasets/kidney_disease/Kidney-Disease.csv'
 data = np.genfromtxt(file_dir, delimiter=',', dtype="|S10")
 
-np.delete(data, 1, 0)   # Remove column labels
+data = np.delete(data, (0), axis=0)   # Remove column
 labels = data[:,24]
 
-'''
 # Split data into train and test sets
 np.random.shuffle(labels)                   # Randomly shuffle the data
 train, test = np.array_split(labels, 2)     # Split to train and test
@@ -27,14 +27,18 @@ train, test = np.array_split(labels, 2)     # Split to train and test
 # Frequency Computations
 train_freq = collections.Counter(train)     # Generate frequency counts
 result = train_freq.most_common(1)[0][0]    # Get the most common frequency
-'''
-print 'Training Set Frequency: ' + str(labels)
+
+# Display Training Model
+print '[ZeroR Generated Model]'
+print 'Training Set Frequency: ' + str(train_freq)
 print 'Result (Mode): ' + str(result)
 print
 
 # Test against test set
 test_freq = collections.Counter(test)
-acc = test_freq[result] / float(len(test_freq))
+acc = test_freq[result] / float(len(test))
 
+# Display Test Results
+print '[ZeroR Tested Models]'
 print 'Test Set Frequency: ' + str(test_freq)
-print 'Accuracy: ' + str(acc) + "%"
+print 'Accuracy: ' + str(acc)  # Should be ~28% if done correctly
