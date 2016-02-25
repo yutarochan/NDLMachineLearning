df534c5ef6b427d633475cf98cb16a27aefdf7aa
Yuya Ong <yutarochan@client-130-203-167-224.mobility.psu.edu>
2016-02-25 14:58:09 -0500

    Add improved neural network code - backpropagation algorithm

--- Classification/ZeroR/zeroRDemo.py
+++ Classification/ZeroR/zeroRDemo.py
@@ -1,16 +1,11 @@
-'''
-ZeroR Classification
-Demonstration of a ZeroR classification process.
-'''
 import numpy as np
 import collections
 
-# Kidney Disease Sample Dataset
 file_dir = 'Kidney-Disease.csv'
 data = np.genfromtxt(file_dir, delimiter=',', dtype='|S10')
 
-data = np.delete(data, (0), axis=0)   # Remove column
-labels = data[:, 24]
+data = np.delete(data, (0), axis=0)
+labels = data[:,24]
 
 # Split data into train and test sets
 np.random.shuffle(labels)                   # Randomly shuffle the data
@@ -18,19 +13,10 @@ train, test = np.array_split(labels, 2)     # Split to train and test
 
 # Frequency Computations
 train_freq = collections.Counter(train)     # Generate frequency counts
-result = train_freq.most_common(1)[0][0]    # Get the most common frequency
+result = train_freq.most_common(1)[0][0]
 
 # Display Training Model
 print '[ZeroR Generated Model]'
 print 'Training Set Frequency: ' + str(train_freq)
 print 'Result (Mode): ' + str(result)
 print
-
-# Test against test set
-test_freq = collections.Counter(test)
-acc = test_freq[result] / float(len(test))
-
-# Display Test Results
-print '[ZeroR Tested Models]'
-print 'Test Set Frequency: ' + str(test_freq)
-print 'Accuracy: ' + str(acc)  # Should be ~28% if done correctly
